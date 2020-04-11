e153c7344f90924cc69c2bb36041aec030146cdc
Thanh Duc Nguyen <thanhduc.nguyen@episerver.com>
2020-04-12 03:20:38 +0700

    Custom view engine

---
+++ WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace WorkingWithRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string[] names = { "Apple", "Orange", "Pear" };
+            return View(names);
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
